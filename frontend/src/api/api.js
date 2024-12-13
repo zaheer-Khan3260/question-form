72383af4ed1d9af416420c20c690fb8bf05357df
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-12-13 17:25:57 +0530

    update the api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_SERVER_URL, 
+    baseURL: "https://question-form-api.vercel.app/api", 
     withCredentials: true 
 });
   
