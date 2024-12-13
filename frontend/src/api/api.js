fd4cd77d00ead7ff5541e982409976dfe4655dd5
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-12-13 19:00:14 +0530

    api update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,12 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_SERVER_URL, 
-    withCredentials: true 
+    baseURL: "https://question-form-rcqc.onrender.com/api", 
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'https://rainbow-vacherin-596f62.netlify.app'
+      }
 });
   
 export default api;
